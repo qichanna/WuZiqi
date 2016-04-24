acf77291f453b57138153a6984405c247ba6b739
qichanna <qichanna@126.com>
2016-04-24 23:28:42 +0800

    加入ontouchEvent事件

--- src/main/java/com/liqi/wuziqi/WuZiQiPanel.java
+++ src/main/java/com/liqi/wuziqi/WuZiQiPanel.java
@@ -1,8 +1,16 @@
 package com.liqi.wuziqi;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * Created by liqi on 2016/4/24.
@@ -13,9 +21,32 @@ public class WuZiQiPanel extends View{
     private float mLineHeight;
     private int MAX_LINE = 10;
 
+    private Paint mPaint = new Paint();
+
+    private Bitmap mWhitePiece;
+    private Bitmap mBlackPiece;
+
+    private  float ratioPieceOfLineHeight = 3 * 1.0f / 4;
+
+    //白棋先手,当前轮到白棋
+    private boolean mIsWhite = true;
+    private ArrayList<Point> mWhiteArray = new ArrayList<>();
+    private ArrayList<Point> mBlackArray = new ArrayList<>();
+
     public WuZiQiPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(0x44ff0000);
+        init();
+    }
+
+    private void init() {
+        mPaint.setColor(0x88000000);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+
+        mWhitePiece = BitmapFactory.decodeResource(getResources(),R.drawable.stone_w2);
+        mBlackPiece = BitmapFactory.decodeResource(getResources(),R.drawable.stone_b1);
     }
 
     @Override
@@ -36,5 +67,81 @@ public class WuZiQiPanel extends View{
             width = widthSize;
         }
         setMeasuredDimension(width,width);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mPanelWidth = w;
+        mLineHeight = mPanelWidth * 1.0f / MAX_LINE;
+
+        int pieceWidth = (int) (mLineHeight * ratioPieceOfLineHeight);
+
+        mWhitePiece = Bitmap.createScaledBitmap(mWhitePiece,pieceWidth,pieceWidth,false);
+        mBlackPiece = Bitmap.createScaledBitmap(mBlackPiece,pieceWidth,pieceWidth,false);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if(action == MotionEvent.ACTION_UP){
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            Point p = getValidPoint(x,y);
+            if(mWhiteArray.contains(p) || mBlackArray.contains(p)){
+                return false;
+            }
+
+            if(mIsWhite){
+                mWhiteArray.add(p);
+            }else{
+                mBlackArray.add(p);
+            }
+            invalidate();
+            mIsWhite = !mIsWhite;
+        }
+        return true;
+    }
+
+    private Point getValidPoint(int x, int y) {
+        return new Point((int)(x / mLineHeight),(int)(y / mLineHeight));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawBoard(canvas);
+        drawPieces(canvas);
+    }
+
+    private void drawPieces(Canvas canvas) {
+        for(int i = 0, n = mWhiteArray.size(); i < n; i++){
+            Point whitePoint = mWhiteArray.get(i);
+            canvas.drawBitmap(mWhitePiece,
+                    (whitePoint.x + (1 - ratioPieceOfLineHeight) /2) * mLineHeight,
+                    (whitePoint.y + (1 - ratioPieceOfLineHeight) /2) * mLineHeight,null);
+        }
+
+        for(int i = 0, n = mBlackArray.size(); i < n; i++){
+            Point blackPoint = mBlackArray.get(i);
+            canvas.drawBitmap(mBlackPiece,
+                    (blackPoint.x + (1 - ratioPieceOfLineHeight) /2) * mLineHeight,
+                    (blackPoint.y + (1 - ratioPieceOfLineHeight) /2) * mLineHeight,null);
+        }
+    }
+
+    private void drawBoard(Canvas canvas) {
+        int w = mPanelWidth;
+        float lineHeight = mLineHeight;
+
+        for(int i = 0; i < MAX_LINE; i++){
+            int startX = (int) (lineHeight / 2);
+            int endX = (int) (w - lineHeight / 2);
+            int y = (int) ((0.5 + i) * lineHeight);
+            canvas.drawLine(startX,y,endX,y,mPaint);
+            canvas.drawLine(y,startX,y,endX,mPaint);
+        }
     }
 }
