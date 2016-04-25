84d11cc5b916f364e8ab2bd189f6a87f58e038cc
liqi7 <liqi7@lenovo.com>
2016-04-25 13:56:06 +0800

    五子连珠胜负判断，view的存储，actionbar再来一局添加

--- src/main/java/com/liqi/wuziqi/MainActivity.java
+++ src/main/java/com/liqi/wuziqi/MainActivity.java
@@ -1,13 +1,34 @@
 package com.liqi.wuziqi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private  WuZiQiPanel wuziqiPanel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wuziqiPanel = (WuZiQiPanel) findViewById(R.id.wzqpanel);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_again){
+            wuziqiPanel.start();
+        }
+        return true;
     }
 }
--- src/main/java/com/liqi/wuziqi/WuZiQiPanel.java
+++ src/main/java/com/liqi/wuziqi/WuZiQiPanel.java
@@ -6,9 +6,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,7 @@ public class WuZiQiPanel extends View{
     private int mPanelWidth;
     private float mLineHeight;
     private int MAX_LINE = 10;
+    private int MAX_COUNT_IN_LINE = 5;
 
     private Paint mPaint = new Paint();
 
@@ -33,9 +37,12 @@ public class WuZiQiPanel extends View{
     private ArrayList<Point> mWhiteArray = new ArrayList<>();
     private ArrayList<Point> mBlackArray = new ArrayList<>();
 
+    private boolean mIsGameOver;
+    private boolean mIsWhiteWinner;
+
     public WuZiQiPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setBackgroundColor(0x44ff0000);
+//        setBackgroundColor(0x44ff0000);
         init();
     }
 
@@ -84,6 +91,9 @@ public class WuZiQiPanel extends View{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if(mIsGameOver){
+            return false;
+        }
         int action = event.getAction();
         if(action == MotionEvent.ACTION_UP){
             int x = (int) event.getX();
@@ -114,6 +124,172 @@ public class WuZiQiPanel extends View{
         super.onDraw(canvas);
         drawBoard(canvas);
         drawPieces(canvas);
+        checkGameOver();
+    }
+
+    private void checkGameOver() {
+        boolean whiteWin = checkFiveLine(mWhiteArray);
+        boolean blackWin = checkFiveLine(mBlackArray);
+
+        if(whiteWin || blackWin){
+            mIsGameOver = true;
+            mIsWhiteWinner = whiteWin;
+
+            String text = mIsWhiteWinner ? "白旗胜利" : "黑棋胜利";
+            Toast.makeText(getContext(),text,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkFiveLine(ArrayList<Point> points) {
+        for(Point p : points){
+            int x = p.x;
+            int y = p.y;
+
+           boolean win = checkHorizontal(x,y,points);
+            if(win) return true;
+            win = checkVertical(x,y,points);
+            if(win) return true;
+            win = checkLeftDiagonal(x,y,points);
+            if(win) return true;
+            win = checkRightDiagonal(x,y,points);
+            if(win) return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断x,y位置的棋子，是否横向有相邻的五个一致
+     * @param x
+     * @param y
+     * @param points
+     * @return
+     */
+    private boolean checkHorizontal(int x, int y, ArrayList<Point> points) {
+        int count = 1;
+        //左
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x - i,y))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x + i,y))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        return false;
+    }
+
+    /**
+     * 判断x,y位置的棋子，是否纵向有相邻的五个一致
+     * @param x
+     * @param y
+     * @param points
+     * @return
+     */
+
+    private boolean checkVertical(int x, int y, ArrayList<Point> points) {
+        int count = 1;
+        //上
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x ,y - i))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x,y + i))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        return false;
+    }
+
+    /**
+     * 判断x,y位置的棋子，是否左斜有相邻的五个一致
+     * @param x
+     * @param y
+     * @param points
+     * @return
+     */
+
+    private boolean checkLeftDiagonal(int x, int y, ArrayList<Point> points) {
+        int count = 1;
+        //上
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x - i ,y + i))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x + i,y - i))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        return false;
+    }
+
+    /**
+     * 判断x,y位置的棋子，是否右斜有相邻的五个一致
+     * @param x
+     * @param y
+     * @param points
+     * @return
+     */
+
+    private boolean checkRightDiagonal(int x, int y, ArrayList<Point> points) {
+        int count = 1;
+        //上
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x - i ,y - i))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        for(int i = 1; i < MAX_COUNT_IN_LINE;i++){
+            if(points.contains(new Point(x + i,y + i))){
+                count++;
+            }else {
+                break;
+            }
+        }
+
+        if(count == MAX_COUNT_IN_LINE) return true;
+
+        return false;
     }
 
     private void drawPieces(Canvas canvas) {
@@ -143,5 +319,40 @@ public class WuZiQiPanel extends View{
             canvas.drawLine(startX,y,endX,y,mPaint);
             canvas.drawLine(y,startX,y,endX,mPaint);
         }
+    }
+
+    public void start(){
+        mWhiteArray.clear();
+        mBlackArray.clear();
+        mIsGameOver = false;
+        mIsWhiteWinner = false;
+        invalidate();
+    }
+
+    private static final String INSTANCE = "instance";
+    private static final String INSTANCE_GAME_OVER = "instance_game_over";
+    private static final String INSTANCE_WHITE_ARRAY = "instance_white_array";
+    private static final String INSTANCE_BLACK_ARRAY = "instance_black_array";
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(INSTANCE,super.onSaveInstanceState());
+        bundle.putBoolean(INSTANCE_GAME_OVER,mIsGameOver);
+        bundle.putParcelableArrayList(INSTANCE_WHITE_ARRAY,mWhiteArray);
+        bundle.putParcelableArrayList(INSTANCE_BLACK_ARRAY,mBlackArray);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if(state instanceof Bundle){
+            Bundle bundle = (Bundle) state;
+            mIsGameOver = bundle.getBoolean(INSTANCE_GAME_OVER);
+            mWhiteArray = bundle.getParcelableArrayList(INSTANCE_WHITE_ARRAY);
+            mBlackArray = bundle.getParcelableArrayList(INSTANCE_BLACK_ARRAY);
+            super.onRestoreInstanceState(bundle.getParcelable(INSTANCE));
+        }
+        super.onRestoreInstanceState(state);
     }
 }
